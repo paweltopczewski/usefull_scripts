4a7a727a3b4a93fca38cc190be90af9c4eb914b9
paweltopczewski <pawel.topczewski@gmail.com>
2021-02-13 13:45:31 +0100

    Update dhcp_lease_vsx.sh

--- dhcp_lease_vsx.sh
+++ dhcp_lease_vsx.sh
@@ -1,1 +1,9 @@
-#
+#!/bin/bash
+if [[ -e /etc/profile.d/CP.sh ]]; then source /etc/profile.d/CP.sh; fi
+if [[ -e /etc/profile.d/vsenv.sh ]]; then source /etc/profile.d/vsenv.sh; fi
+vsenv 2
+dhclient -1 eth1
+dhclient eth1
+vsenv 1
+dhclient -1 eth2
+dhclient eth2
